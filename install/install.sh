844b31a324787e4e0cf2cf8547e3dd6cbd5efbbf
Shohei Nakajima <nakajimashouhei@gmail.com>
2016-02-27 08:54:44 +0900

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -26,10 +26,10 @@ if [ "$NC3DIR" = "" ]; then
 	echo "Chage directory /var/www/MyShell/install"
 	exit
 fi
-if [ ! "$CURDIR" = "/var/www/MyShell/install" ]; then
-	echo "Chage directory /var/www/MyShell/install"
-	exit
-fi
+#if [ ! "$CURDIR" = "/var/www/MyShell/install" ]; then
+#	echo "Chage directory /var/www/MyShell/install"
+#	exit
+#fi
 
 if [ "$2" = "docs" ]; then
 	SKIPDOCS=0; export SKIPDOCS
